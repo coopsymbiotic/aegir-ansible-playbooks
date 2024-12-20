6706ee7cce271bd87daaec4073680fbfb99ce980
Mathieu Lu <mathieu@symbiotic.coop>
2024-12-19 19:14:24 -0500

    Backdrop settings.local.php: generate trusted_host_patterns

--- aegir/site/templates/Backdrop/settings.local.php
+++ aegir/site/templates/Backdrop/settings.local.php
@@ -3,3 +3,9 @@
 # Used by 'bee uli'
 global $base_url;
 $base_url = 'https://{{ inventory_hostname }}';
+
+# https://docs.backdropcms.org/documentation/trusted-host-settings
+# Aliases are currently not supported by Aegir because we assume redirects
+$settings['trusted_host_patterns'] = [
+  '^{{ inventory_hostname }}$',
+];
